7b2fb9636c664391953f2755359206d72c7ab837
Greg Hamilton <greghamilton@cmail.carleton.ca>
2019-11-09 18:28:00 -0500

    flutter environment

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\greg\Codebase\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\greg\Codebase\audioplayers\example"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\greg\Codebase\flutter\bin\cache\artifacts\engine\ios"
